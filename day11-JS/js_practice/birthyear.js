fbdcaf2a4d7baa6605f0c698c07c1e8087b24750
Vojtech Vanek <vanevo00@gmail.com>
2019-09-25 08:52:37 +0200

    Revert "tuesday commit - JS practice"
    This reverts commit 24d1bbdc4f8b44c7aee793987ec6e00c133de71e.

--- day11-JS/js_practice/birthyear.js
+++
@@ -1,2 +0,0 @@
-const birth_year = parseInt(prompt("What year were you born in?"));
-alert(`you are currently ${2019 - birth_year}`);
--- day11-JS/js_practice/currency_exchange.js
+++
@@ -1,3 +0,0 @@
-const exchange_rate = 25.695;
-const euros = parseInt(prompt("Insert amount to be converted to CZK:"));
-alert(`EUR ${euros} is CZK ${Math.round(exchange_rate * euros)}`);
