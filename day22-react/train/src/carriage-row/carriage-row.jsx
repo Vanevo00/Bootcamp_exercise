209dea799de899dd77e8ad0cd0f12af27a10def1
Vojtech Vanek <vanevo00@gmail.com>
2019-10-09 16:38:43 +0200

    day 23 - php basics

--- day22-react/train/src/carriage-row/carriage-row.jsx
+++ day22-react/train/src/carriage-row/carriage-row.jsx
@@ -2,32 +2,47 @@ import React from 'react';
 import Seat from "../seat/seat.jsx";
 
 export default class CarriageRow extends React.Component {
-  checkAvailability(seat) {
-    if (seat === "t") {
-      return "free";
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      availability: this.props.availability,
     }
-    return "occupied";
   }
+  
+  checkAvailability(seat) {
+    switch (seat) {
+      case "t":
+        return "free";
+        break;
+      case "s":
+        return "selected";
+        break;
+      default:
+        return "occupied";
+    }
+  }
+
+  
   
   render() {
     return (
-      <div class="carriage-row">
+      <div className="carriage-row">
         <Seat
-          availability={this.checkAvailability(this.props.availability[0])}
+          availability={this.checkAvailability(this.state.availability[0])}
           seatNo={this.props.row * 4 + 1}
-          onClick={this.handleClick}
         />
         <Seat
-          availability={this.checkAvailability(this.props.availability[1])}
+          availability={this.checkAvailability(this.state.availability[1])}
           seatNo={this.props.row * 4 + 2}
         />
-        <div class="aisle-spacer"></div>
+        <div className="aisle-spacer"></div>
         <Seat
-          availability={this.checkAvailability(this.props.availability[2])}
+          availability={this.checkAvailability(this.state.availability[2])}
           seatNo={this.props.row * 4 + 3}
         />
         <Seat
-          availability={this.checkAvailability(this.props.availability[3])}
+          availability={this.checkAvailability(this.state.availability[3])}
           seatNo={this.props.row * 4 + 4}
         />
       </div>
---
+++ day23-php-basics/first_practice/hello.php
@@ -0,0 +1,7 @@
+<?php 
+  
+  echo "<h1>Hello world!</h1>";
+  
+?>
+
+<h2 style="color: red">pes</h2>
